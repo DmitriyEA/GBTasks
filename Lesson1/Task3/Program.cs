fffe0f32b0faf6c7b6e0bf9d91b5fcb6fef20aa0
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 06:31:36 +0300

    add Task3 (-n to n)

---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+Write("Введите число: ");
+int a = Convert.ToInt32(ReadLine());
+int n = -(a);
+
+if (a < 0)
+{
+    while (n > a)
+    {
+        Write($"{a} ");
+        a = a + 1;
+    }
+}
+if (a > 0)
+{
+    while (n <= a)
+    {
+        Write($"{n} ");
+        n = n + 1;
+    }
+}
+else WriteLine("null");
