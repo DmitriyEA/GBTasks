bd618832bad818d40c9b755616148f17449d5358
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 06:54:19 +0300

    add Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(ReadLine());
+
+if (a >= 100 && a <=999)
+{
+    WriteLine(a%10);
+}
+else
+{
+    WriteLine("Введено не трехзначное число.");
+}
