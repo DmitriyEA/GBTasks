192f1737faeef629971461d637e607bd94b6562f
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 11:12:38 +0300

    add HW1 task6

---
+++ Homework1/hw1_task6/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Write("Введите число ");
+int a = Convert.ToInt32(ReadLine());
+int ostatok = a%2;
+
+if (ostatok == 0)
+{
+    WriteLine($"Число {a} является четным.");
+}
+else
+WriteLine($"Число {a} является нечетным.");
