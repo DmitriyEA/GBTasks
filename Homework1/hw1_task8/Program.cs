1331a7fe7cea68d7ed41d017dbf5e9fe250a8707
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 11:27:43 +0300

    add HW1 task8

---
+++ Homework1/hw1_task8/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Write("Введите число N: ");
+int N = Convert.ToInt32(ReadLine());
+int count = 2;
+
+WriteLine($"Четные числа от 1 до {N}:");
+while (count <= N)
+{
+    Write($"{count}, ");
+    count = count + 2;
+}
