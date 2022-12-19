646733a0c7d40089a661ab2d6ec3df677d18e5d1
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 08:37:23 +0300

    add HW1 task2

---
+++ Homework1/hw1_task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+Write("Введите число а:");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите число b:");
+int b = Convert.ToInt32(ReadLine());
+
+if (a > b)
+{
+    WriteLine("a больше b");
+}
+if (a < b)
+{
+    WriteLine("b больше a");
+}
+else            
+{
+    WriteLine("числа равны");
+}
