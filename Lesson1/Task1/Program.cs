3fe69b6b62624885b936360ae9351d3ff4dbfb8f
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 03:46:50 +0300

    folder organization

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Является ли число а квадратом числа b?");
+WriteLine("Введите число а: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите число b: ");
+int b = int.Parse(ReadLine()!);
+
+if (b * b == a)
+{
+    WriteLine("ДА");
+}
+else
+{
+    WriteLine("НЕТ");
+}
