4f6fd4c2b8d679a206168a084941514af2d7c606
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 04:20:57 +0300

    add Task2 (days of week)

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+
+Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(ReadLine()!);
+
+if (day > 0 && day < 8)
+{
+    if (day == 1)
+    {
+        Write("Это понедельник");
+    }
+
+    if (day == 2)
+    {
+        Write("Это вторник");
+    }
+
+    if (day == 3)
+    {
+        Write("Это среда");
+    }
+
+    if (day == 4)
+    {
+        Write("Это четверг");
+    }
+
+    if (day == 5)
+    {
+        Write("Это пятница");
+    }
+
+    if (day == 6)
+    {
+        Write("Это суббота");
+    }
+
+    if (day == 7)
+    {
+        Write("Это воскресенье");
+    }
+}
+else
+{
+Write("Такого дня недели нет.");
+}
