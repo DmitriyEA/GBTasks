d9eeecc930d0cc387ec0c9bb885208f29e2ea531
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-19 09:44:44 +0300

    add HW1 task4

---
+++ Homework1/hw1_task4/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+WriteLine("Введите число a: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите число b ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine("Введите число с ");
+int c = Convert.ToInt32(ReadLine());
+
+if (a > b && a > c)
+{
+    WriteLine($"Максимальным является число а = {a}");
+}
+if (b > a && b >c)
+{
+    WriteLine($"Максимальным является число b = {b}");
+}
+if (c > a && c > b)
+{
+    WriteLine($"Максимальным является число с = {c}");
+}
