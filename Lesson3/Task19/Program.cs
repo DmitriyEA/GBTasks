a3f6de3faf537c838ed1f0dc3b49e18d1859073f
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-22 21:16:33 +0300

    add Lesson3

---
+++ Lesson3/Task19/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("Введите х ");
+int x = Convert.ToInt32(ReadLine());
+WriteLine("Введите y ");
+int y = Convert.ToInt32(ReadLine());
+
+if (x != 0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        WriteLine("Точка находится в первой четверти");
+    }
+    else if (x < 0 && y > 0)
+    {
+        WriteLine("Точка находится во второй четверти");
+    }
+    else if (x < 0 && y < 0)
+    {
+        WriteLine("Точка находится в третьей четверти");
+    }
+    else if (x > 0 && y < 0)
+    {
+        WriteLine("Точка находится в четвертой четверти");
+    }
+else
+{
+    WriteLine("Точка попадает на ось координат");
+}
+}
