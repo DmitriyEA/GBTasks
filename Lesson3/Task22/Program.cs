e2e93ab94ba8ab95ac2562a0eadbd77ee3e58da2
Dmitriy Efimov <fimafoxy@gmail.com>
2022-12-22 22:01:43 +0300

    add Tasks

---
+++ Lesson3/Task22/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    WriteLine(count * count);
+    count++;
+}
