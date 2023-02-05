cc719e534e6392abcec0588ddcc7da3ab6c336e9
Dmitriy Efimov <fimafoxy@gmail.com>
2023-02-05 12:27:53 +0300

    add Home Work 6

---
+++ Homework6/Task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+
+using static System.Console;
+Clear();
+
+Write("Введите размер массива: ");
+int SizeArray = Convert.ToInt32(ReadLine());
+
+int[] array = GetArray(SizeArray);
+WriteLine();
+PrintArray(array);
+WriteLine();
+WriteLine($"Чисел, больше нуля: {compareNumbers(array)}");
+
+
+
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr1)
+{
+    Write("[");
+    for (int i = 0; i < arr1.Length - 1; i++)
+    {
+        Write($"{arr1[i]}, ");
+    }
+    Write($"{arr1[arr1.Length - 1]}]");
+}
+
+int compareNumbers(int[] inArray)
+{
+    int count = 0;
+    foreach (var a in inArray)
+    {
+        if (a > 0) count++;
+    }
+    return count;
+}
---
+++ Homework6/Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+using static System.Console;
+Clear();
+
+Write("Введите число b1: ");
+int b1 = int.Parse(ReadLine()!);
+Write("Введите число k1: ");
+int k1 = int.Parse(ReadLine()!);
+Write("Введите число b2: ");
+int b2 = int.Parse(ReadLine()!);
+Write("Введите число k2: ");
+int k2 = int.Parse(ReadLine()!);
+
+FindLineCrossing(b1, k1, b2, k2);
+
+void FindLineCrossing(double num11, double num12, double num21, double num22)
+{
+    double x = (num21 - num11) / (num12 - num22);
+    double y = num12 * x + num11;
+    WriteLine($"x = {x}, y = {y}");
+}
